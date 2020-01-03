b3abcb5c351f4510f52ead49776c8517f4ad3a78
Josh Bryson <josh@bryson.me.uk>
2020-01-03 00:10:03 +0000

    User and Company classes created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('A starting point...')
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+
+console.log('User', user);
+console.log('Company', company);
